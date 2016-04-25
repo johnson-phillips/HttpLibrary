5ecda873f039ac874089e3c8b3d72788ce6b1808
johnson_phillips <johnson.phillips@mheducation.com>
2016-04-25 14:59:00 -0400

    Http Library for basic http get and post. Supports AWS SigV4 authorization

---
+++ src/test/java/HttpTest.java
@@ -0,0 +1,52 @@
+import http.HTTPRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by johnson_phillips on 4/25/16.
+ */
+public class HttpTest {
+
+    HTTPRequest request = new HTTPRequest();
+    HttpResponse response = null;
+    int code;
+    String result;
+
+    @Test(description="Test Http Get")
+    public void HttpGetTest()
+    {
+        try
+        {
+           response = request.GetData("http://httpbin.org/get");
+            code = response.getStatusLine().getStatusCode();
+            Assert.assertEquals(code,200);
+            result = EntityUtils.toString(response.getEntity());
+            System.out.print(result);
+        }
+        catch (Exception ex)
+        {
+            System.out.print(ex);
+            Assert.fail();
+        }
+    }
+
+    @Test(description="Test Http Post")
+    public void HttpPostTest()
+    {
+        try
+        {
+            response = request.PostData("http://httpbin.org/post","Test Data");
+            code = response.getStatusLine().getStatusCode();
+            Assert.assertEquals(code,200);
+            result = EntityUtils.toString(response.getEntity());
+            System.out.print(result);
+        }
+        catch (Exception ex)
+        {
+            System.out.print(ex);
+            Assert.fail();
+        }
+    }
+}
